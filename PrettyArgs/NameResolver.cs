3e0ee172af25f8d8ff84b6810e5dfb799f5e4150
Oscar Marcusson <62374937+OscarMarcusson@users.noreply.github.com>
2023-11-25 22:40:07 +0100

    Create name resolver for automatic flag or option names

---
+++ PrettyArgs/NameResolver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("Tests")]
+namespace PrettyArgs
+{
+	internal static class NameResolver
+	{
+		public static string ResolveLongNameFromVariableName(string name)
+		{
+			var output = new StringBuilder(name.Length + 6);
+			output.Append("--");
+			var last = '-';
+			for (int i = 0; i < name.Length; i++)
+			{
+				if (char.IsDigit(last) || char.IsUpper(last))
+				{
+					output.Append('-');
+					last = '-';
+				}
+
+				// Catch any `-` or `_` that may already exist for whatever reason, ensure we only get a single `-`
+				if (name[i] == '-' || name[i] == '_')
+				{
+					if (last != '-' && last != '_')
+					{
+						last = '-';
+						output.Append(last);
+					}
+				}
+				// Uppercases are always lowercased and prefixed as `-c`
+				else if (char.IsUpper(name[i]))
+				{
+					ReadWhile(char.IsUpper, ref i);
+				}
+				// Digits are always inserted as `-c-`
+				else if (char.IsDigit(name[i]))
+				{
+					ReadWhile(char.IsDigit, ref i);
+				}
+				// Everything else is just printed as is
+				else
+				{
+					last = name[i];
+					output.Append(last);
+					if(last == 'e')
+					{ 
+					}
+				}
+			}
+
+			return output.ToString().TrimEnd('-');
+
+
+			void ReadWhile(Func<char, bool> condition, ref int i)
+			{
+				if (last != '-')
+					output.Append('-');
+
+				for (; i < name.Length; i++)
+				{
+					if (!condition(name[i]))
+					{
+						i--;
+						break;
+					}
+					last = char.ToLower(name[i]);
+					output.Append(last);
+				}
+			}
+		}
+	}
+}
---
+++ Tests/Name_resolver.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrettyArgs;
+
+namespace Tests
+{
+	[TestClass]
+	public class Name_resolver
+	{
+		[TestMethod]
+		[DataRow("example", "--example")]
+		[DataRow("snakeCaseName", "--snake-case-name")]
+		[DataRow("PascalCaseName", "--pascal-case-name")]
+		public void Letters_only(string input, string expected)
+		{
+			var output = NameResolver.ResolveLongNameFromVariableName(input);
+			Assert.AreEqual(expected, output);
+		}
+
+		[TestMethod]
+		[DataRow("example1", "--example-1")]
+		[DataRow("snakeCase123Name", "--snake-case-123-name")]
+		[DataRow("PascalCase123Name", "--pascal-case-123-name")]
+		public void Letters_and_digits(string input, string expected)
+		{
+			var output = NameResolver.ResolveLongNameFromVariableName(input);
+			Assert.AreEqual(expected, output);
+		}
+
+		[TestMethod]
+		[DataRow("example-1-", "--example-1")]
+		[DataRow("example---1", "--example-1")]
+		[DataRow("----example-1-", "--example-1")]
+		[DataRow("snake----Case123--Name-", "--snake-case-123-name")]
+		[DataRow("Pascal----Case123--Name--", "--pascal-case-123-name")]
+		public void Hyphens_are_trimmed_to_one(string input, string expected)
+		{
+			var output = NameResolver.ResolveLongNameFromVariableName(input);
+			Assert.AreEqual(expected, output);
+		}
+	}
+}
