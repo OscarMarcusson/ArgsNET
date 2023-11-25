89a9dd852e346a8f485df833ac9d27625ef6c632
Oscar Marcusson <62374937+OscarMarcusson@users.noreply.github.com>
2023-11-25 19:55:20 +0100

    Add flags and options tests for empty output

--- Tests/Empty_output_class.cs
+++ Tests/Empty_output_class.cs
@@ -13,6 +13,28 @@ namespace Tests
 			Assert.AreEqual(0, errors.Length, "Did not expect any parse errors");
 		}
 
+		[TestMethod]
+		[DataRow("-h")]
+		[DataRow("-v -h")]
+		[DataRow("--verbose-version")]
+		[DataRow("--verbose-help")]
+		public void Flags_return_error(params string[] args)
+		{
+			_ = Arguments.Parse<Empty>(args, out var errors);
+			Assert.AreEqual(1, errors.Length, "Expected one error");
+		}
+
+		[TestMethod]
+		[DataRow("-o", "test")]
+		[DataRow("-o=test")]
+		[DataRow("--verbose-output test")]
+		[DataRow("--verbose-output=test")]
+		public void Options_return_error(params string[] args)
+		{
+			_ = Arguments.Parse<Empty>(args, out var errors);
+			Assert.AreEqual(1, errors.Length, "Expected one error");
+		}
+
 		class Empty { }
 	}
 }
